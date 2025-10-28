6d4e5b6cb94f176d0e26a40ecfcfac33d4f00871
Omar Elmanzalawy <omarmanz2002@gmail.com>
2025-10-28 20:57:30 +0300

    add animation interval

--- lib/explicit_examples/list_animation.dart
+++ lib/explicit_examples/list_animation.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ListAnimation extends StatelessWidget {
+class ListAnimation extends StatefulWidget {
   const ListAnimation({super.key});
+
+  @override
+  State<ListAnimation> createState() => _ListAnimationState();
+}
+
+class _ListAnimationState extends State<ListAnimation> with SingleTickerProviderStateMixin{
+
+  late AnimationController animationController;
+  late List<Animation<Offset>> offsetAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this,duration: Duration(milliseconds: 700));
+    // offsetAnimation = Tween(begin: Offset(-1, 0), end: Offset.zero).animate(animationController);
+    offsetAnimation = List.generate(5, (index) => Tween(begin: Offset(-1,0),end: Offset.zero).animate(CurvedAnimation(
+      parent: animationController,
+       curve: Interval(
+        index / 5,
+         (index + 1) / 5
+         )),
+
+       ));
+
+
+    super.initState();
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +40,22 @@ class ListAnimation extends StatelessWidget {
       body: ListView.builder(
         itemCount: 5,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Hello World, Rivaan. ${index.toString()}'),
+          return SlideTransition(
+            position: offsetAnimation[index],
+            child: ListTile(
+              title: Text('Hello World, Rivaan. ${index.toString()}'),
+            ),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if(animationController.status == AnimationStatus.completed){
+            animationController.reverse();
+          }else{
+            animationController.forward();
+          }
+        },
         child: const Icon(Icons.done),
       ),
     );
