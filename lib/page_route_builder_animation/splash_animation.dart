97da295c8de8c5a7c1c6d5e9806cd6e22adb1340
Omar Elmanzalawy <omarmanz2002@gmail.com>
2025-10-30 16:56:29 +0300

    finish page route transition

--- lib/page_route_builder_animation/splash_animation.dart
+++ lib/page_route_builder_animation/splash_animation.dart
@@ -1,23 +1,71 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class SplashAnimation extends StatelessWidget {
+class SplashAnimation extends StatefulWidget {
   const SplashAnimation({super.key});
+
+  @override
+  State<SplashAnimation> createState() => _SplashAnimationState();
+}
+
+class _SplashAnimationState extends State<SplashAnimation> with SingleTickerProviderStateMixin{
+
+  late AnimationController animationController;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+    scaleAnimation = Tween(begin: 1.0,end: 10.0).animate(animationController);
+    super.initState();
+
+    animationController.addListener((){
+      print("animation is changing");
+      print(scaleAnimation.value);
+    });
+
+    animationController.addStatusListener((status){
+      // print(status);
+      if(status == AnimationStatus.completed){
+        Navigator.push(context,PageRouteBuilder(pageBuilder:(context, animation, secondaryAnimation) {
+          return Destination();
+        },
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeTransition(opacity: animation,child: child,);
+        },
+        ) ).whenComplete((){animationController.reset();});
+
+        // Timer(Duration(milliseconds: 300), (){
+        //   animationController.reset();
+        // });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-            shape: BoxShape.circle,
-          ),
-          child: Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue,
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: GestureDetector(
+            onTap: () {
+              animationController.forward();
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                ),
+              ),
             ),
           ),
         ),
